87ba50d91af3dd60dc193603e47527c973dffa7c
Josh <jsterlingvids@gmail.com>
2025-08-08 23:49:07 +0200

    Insta

--- src/components/MasonryGrid.jsx
+++ src/components/MasonryGrid.jsx
@@ -113,7 +113,12 @@ export default function MasonryGrid() {
                     loading="lazy"
                     onError={(e) => { e.currentTarget.style.display = 'none'; }}
                   />
-                  <div className="play-icon">▶</div>
+                  <div className="play-icon">
+  <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="white">
+    <path d="M8 5v14l11-7z"/>
+  </svg>
+</div>
+
                 </div>
               </motion.div>
             );
